98c263c5702def3e2b7f3e0e5d7f56a6fa8618eb
Sakura55Blossom <66422123+Sakura55Blossom@users.noreply.github.com>
2020-12-23 22:22:01 +0000

    more work on fonts javascript

--- script.js
+++ script.js
@@ -1,15 +1,192 @@
+
+/*******Fonts******/
+
+    /**Lobster**/
+
 var lobster = document.getElementById("lobster");
 var lobsterP = document.getElementById("lobster-p");
 
-function showFontName() {
+function showLobster() {
     lobsterP.innerHTML = "Lobster";
-    lobsterP.style.fontSize = "50px";
+    lobsterP.style.fontSize = "60px";
     lobsterP.style.margin = "auto";
+    lobster.style.backgroundColor = "pink";
 };
 
-/*function hideFontName() {
-    lobster.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
-};*/
+function hideLobster() {
+    lobsterP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    lobsterP.style.fontSize = "20px";
+    lobster.style.backgroundColor = "white";
+};
 
-lobster.addEventListener("mouseover", showFontName);
-/*lobster.addEventListener("mouseover", hideFontName);*/
+lobster.addEventListener("mouseover", showLobster);
+lobster.addEventListener("mouseout", hideLobster);
+
+    /**Roboto**/
+
+var roboto = document.getElementById("roboto");
+var robotoP = document.getElementById("roboto-p");
+    
+function showRoboto() {
+        robotoP.innerHTML = "Roboto";
+        robotoP.style.fontSize = "60px";
+        robotoP.style.margin = "auto";
+        roboto.style.backgroundColor = "pink";
+};
+    
+function hideRoboto() {
+        robotoP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+        robotoP.style.fontSize = "20px";
+        roboto.style.backgroundColor = "white";
+};
+    
+roboto.addEventListener("mouseover", showRoboto);
+roboto.addEventListener("mouseout", hideRoboto);
+
+        /**Montserrat**/
+
+var montserrat = document.getElementById("montserrat");
+var montserratP = document.getElementById("montserrat-p");
+
+function showMontserrat() {
+    montserratP.innerHTML = "Montserrat";
+    montserratP.style.fontSize = "60px";
+    montserratP.style.margin = "auto";
+    montserrat.style.backgroundColor = "pink";
+};
+
+function hideMontserrat() {
+    montserratP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    montserratP.style.fontSize = "20px";
+    montserrat.style.backgroundColor = "white";
+};
+
+montserrat.addEventListener("mouseover", showMontserrat);
+montserrat.addEventListener("mouseout", hideMontserrat);
+
+        /**Verdana**/
+
+var verdana = document.getElementById("verdana");
+var verdanaP = document.getElementById("verdana-p");
+        
+function showVerdana() {
+    verdanaP.innerHTML = "Verdana";
+    verdanaP.style.fontSize = "60px";
+    verdanaP.style.margin = "auto";
+    verdana.style.backgroundColor = "pink";
+};
+        
+function hideVerdana() {
+    verdanaP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    verdanaP.style.fontSize = "20px";
+    verdana.style.backgroundColor = "white";
+};
+        
+verdana.addEventListener("mouseover", showVerdana);
+verdana.addEventListener("mouseout", hideVerdana);
+
+        /**Oswald**/
+
+var oswald = document.getElementById("oswald");
+var oswaldP = document.getElementById("oswald-p");
+                
+function showOswald() {
+    oswaldP.innerHTML = "Oswald";
+    oswaldP.style.fontSize = "60px";
+    oswaldP.style.margin = "auto";
+    oswald.style.backgroundColor = "pink";
+};
+                
+function hideOswald() {
+    oswaldP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    oswaldP.style.fontSize = "20px";
+    oswald.style.backgroundColor = "white";
+};
+                
+oswald.addEventListener("mouseover", showOswald);
+oswald.addEventListener("mouseout", hideOswald);
+  
+        /**Dancing-Script**/
+
+var dancingScript = document.getElementById("dancing-script");
+var dancingScriptP = document.getElementById("dancing-script-p");
+                
+function showDancingScript() {
+    dancingScriptP.innerHTML = "Dancing Script";
+    dancingScriptP.style.fontSize = "60px";
+    dancingScriptP.style.margin = "auto";
+    dancingScript.style.backgroundColor = "pink";
+};
+                
+function hideDancingScript() {
+    dancingScriptP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    dancingScriptP.style.fontSize = "20px";
+    dancingScript.style.backgroundColor = "white";
+};
+                
+dancingScript.addEventListener("mouseover", showDancingScript);
+dancingScript.addEventListener("mouseout", hideDancingScript);
+        
+        /**Source Code Pro**/
+
+var sourceCodePro = document.getElementById("source-code-pro");
+var sourceCodeProP = document.getElementById("source-code-pro-p");
+                
+function showSourceCodePro() {
+    sourceCodeProP.innerHTML = "Source Code Pro";
+    sourceCodeProP.style.fontSize = "60px";
+    sourceCodeProP.style.margin = "auto";
+    sourceCodePro.style.backgroundColor = "pink";
+};
+                
+function hideSourceCodePro() {
+    sourceCodeProP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    sourceCodeProP.style.fontSize = "20px";
+    sourceCodePro.style.backgroundColor = "white";
+};
+                
+sourceCodePro.addEventListener("mouseover", showSourceCodePro);
+sourceCodePro.addEventListener("mouseout", hideSourceCodePro);
+
+        /**Luckiest Guy
+
+var verdana = document.getElementById("verdana");
+var verdanaP = document.getElementById("verdana-p");
+                
+function showVerdana() {
+    verdanaP.innerHTML = "Verdana";
+    verdanaP.style.fontSize = "60px";
+    verdanaP.style.margin = "auto";
+    verdana.style.backgroundColor = "pink";
+};
+                
+function hideVerdana() {
+    verdanaP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    verdanaP.style.fontSize = "20px";
+    verdana.style.backgroundColor = "white";
+};
+                
+verdana.addEventListener("mouseover", showVerdana);
+verdana.addEventListener("mouseout", hideVerdana);
+        
+         /**Gloria Hallelujah
+/*
+var verdana = document.getElementById("verdana");
+var verdanaP = document.getElementById("verdana-p");
+        
+function showVerdana() {
+    verdanaP.innerHTML = "Verdana";
+    verdanaP.style.fontSize = "60px";
+    verdanaP.style.margin = "auto";
+    verdana.style.backgroundColor = "pink";
+};
+        
+function hideVerdana() {
+    verdanaP.innerHTML = "Life isn't finding shelter in the storm. It's about learning to dance in the rain.";
+    verdanaP.style.fontSize = "20px";
+    verdana.style.backgroundColor = "white";
+};
+        
+verdana.addEventListener("mouseover", showVerdana);
+verdana.addEventListener("mouseout", hideVerdana);
+*/
